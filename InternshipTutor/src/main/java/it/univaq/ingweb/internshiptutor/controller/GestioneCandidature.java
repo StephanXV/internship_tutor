15563849f863e0befb6ba1ecb67abb3604892b08
Stefano Florio <31825478+StephanXV@users.noreply.github.com>
2019-11-17 17:56:14 +0100

    Gestione candidati templating #2

--- InternshipTutor/src/main/java/it/univaq/ingweb/internshiptutor/controller/GestioneCandidature.java
+++ InternshipTutor/src/main/java/it/univaq/ingweb/internshiptutor/controller/GestioneCandidature.java
@@ -7,6 +7,7 @@ import it.univaq.ingweb.framework.result.TemplateResult;
 import it.univaq.ingweb.framework.security.SecurityLayer;
 import it.univaq.ingweb.internshiptutor.data.dao.InternshipTutorDataLayer;
 import it.univaq.ingweb.internshiptutor.data.model.Candidatura;
+import it.univaq.ingweb.internshiptutor.data.model.OffertaTirocinio;
 import it.univaq.ingweb.internshiptutor.data.model.Utente;
 import javafx.scene.control.Alert;
 
@@ -34,13 +35,16 @@ public class GestioneCandidature extends InternshipTutorBaseController {
     private void action_default(HttpServletRequest request, HttpServletResponse response, int id_ot) 
             throws ServletException, IOException, TemplateManagerException {
         try {
+            OffertaTirocinio ot = ((InternshipTutorDataLayer)request.getAttribute("datalayer")).getOffertaTirocinioDAO().getOffertaTirocinio(id_ot);
+            request.setAttribute("nome_tirocinio", ot.getTitolo());
+            
             List<Candidatura> richieste_cand = ((InternshipTutorDataLayer)request.getAttribute("datalayer")).getCandidaturaDAO().getCandidature(id_ot, 0);
             request.setAttribute("richieste_cand", richieste_cand);
             
             List<Candidatura> cand_accettate = ((InternshipTutorDataLayer)request.getAttribute("datalayer")).getCandidaturaDAO().getCandidature(id_ot, 1);
             request.setAttribute("cand_accettate", cand_accettate);
             
-            List<Candidatura> tiro_terminati = ((InternshipTutorDataLayer)request.getAttribute("datalayer")).getCandidaturaDAO().getCandidature(id_ot, 1);
+            List<Candidatura> tiro_terminati = ((InternshipTutorDataLayer)request.getAttribute("datalayer")).getCandidaturaDAO().getCandidature(id_ot, 2);
             request.setAttribute("tiro_terminati", tiro_terminati);
             
             TemplateResult res = new TemplateResult(getServletContext());
